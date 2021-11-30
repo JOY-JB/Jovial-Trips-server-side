d1772dfae2c55604de48df56386363e8bb0b01e3
JOY BARUA <joybarua0058@gmail.com>
2021-11-29 19:17:18 -0800

    last commit

--- index.js
+++ index.js
@@ -13,29 +13,60 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nzciw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-async function run(){
-    try{
+async function run() {
+    try {
         await client.connect();
         const database = client.db("jovial_trips");
         const eventsCollection = database.collection("events");
+        const bookingCollection = database.collection("booking");
+
         // get all events data
-        app.get('/events', async (req,res)=>{
+        app.get('/events', async (req, res) => {
             const eventsData = eventsCollection.find({});
             const result = await eventsData.toArray();
             res.json(result)
-        })
+        });
         // get a event by id
-        app.get('/event/:id', async (req,res)=>{
+        app.get('/event/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const result = await eventsCollection.findOne(query);
             res.json(result);
+        });
+
+        // get api to find orders  by email
+        app.get('/orders', async (req, res) => {
+            const email = req.query.email;
+            let resultData;
+            if (email) {
+                const query = { email: email };
+                resultData = await bookingCollection.find(query);
+            } else {
+                resultData = await bookingCollection.find({});
+            }
+            const result = await resultData.toArray();
+            // console.log(query);
+            res.json(result);
+        });
+
+        // post req to add Tour booking
+        app.post('/booking', async (req, res) => {
+            const booking = req.body;
+            const result = await bookingCollection.insertOne(booking);
+            res.json(result);
+        })
+        // delete order booking
+        app.delete('/deleteorder/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await bookingCollection.deleteOne(query);
+            res.send(result);
         })
     }
     finally {
         // await client.close();
-      }
-} 
+    }
+}
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
